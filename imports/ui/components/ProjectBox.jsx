c959e1f50bb29f973a5e43eafde019934ee7f675
George Secrieru <george.secrieru@gmail.com>
2016-08-05 20:56:29 -0300

    Added navigation to project list

--- imports/ui/components/ProjectBox.jsx
+++ imports/ui/components/ProjectBox.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { FlowRouter } from 'meteor/kadira:flow-router-ssr';
 
 class ProjectBox extends React.Component {
+
+  constructor() {
+    super();
+
+    this.handleBoxClick = this.handleBoxClick.bind(this);
+  }
+
   render() {
     const { project } = this.props;
     const { tasks = [] } = project;
 
     return (
-      <div className="project-box">
+      <div className="project-box" onClick={ this.handleBoxClick }>
         <div className="project-box__content">
           <h3>{ project.name }</h3>
           <small>{ `${tasks.length} ${tasks.length === 1 ? 'task' : 'tasks'}` }</small>
@@ -17,6 +25,13 @@ class ProjectBox extends React.Component {
       </div>
     );
   }
+
+  handleBoxClick(e) {
+    if (!e.target.matches('i')) {
+      return FlowRouter.go('project-by-id', { id: this.props.project._id });
+    }
+  }
+
 }
 
 ProjectBox.propTypes = {
