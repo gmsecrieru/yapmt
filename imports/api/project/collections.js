7383d87cf81ef3fb81901c983af805944a3ad04b
George Secrieru <george.secrieru@gmail.com>
2016-08-05 20:29:41 -0300

    Allowing mongodb collection operations from client-side; added publication for project tasks

--- imports/api/project/collections.js
+++ imports/api/project/collections.js
@@ -1,10 +1,25 @@
 import { Mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
+/**
+ * Disclaimer:
+ *
+ * For simplicity's sake, we are allowing these operations to be performed
+ * on the client-side with no particular restriction (e.g. logged user, ACLs etc.),
+ * thus afore mentioned operations will be sync'ed with the server automatically.
+ */
+const collectionInteractionRules = {
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+}
+
 const Project = new Mongo.Collection('project');
 Project.attachSchema(new SimpleSchema({
   name: { type: String }
 }));
+Project.allow(Object.assign({}, collectionInteractionRules));
+
 export { Project };
 
 const Task = new Mongo.Collection('task');
@@ -13,9 +28,10 @@ Task.attachSchema(new SimpleSchema({
   'description': { type: String },
   'due_date': { type: Date },
   'owner': { type: String },
-  'completed': { type: Boolean },
-
+  'completed': { type: Boolean }
 }));
+Task.allow(Object.assign({}, collectionInteractionRules));
+
 export { Task }
 
 export default {
--- imports/api/project/server/publications.js
+++ imports/api/project/server/publications.js
@@ -1,7 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 
-import { Project } from '/imports/api/project/collections';
+import { Project, Task } from '/imports/api/project/collections';
 
 export const projectList = Meteor.publish('projectList', () => {
   return Project.find();
+});
+
+export const projectById = Meteor.publish('projectById', (id) => {
+  return Project.find({ _id: id });
+});
+
+export const tasksByProjectId = Meteor.publish('tasksByProjectId', (id) => {
+  return Task.find({ project_id: id }, { sort: { due_date: 1 } });
 });
